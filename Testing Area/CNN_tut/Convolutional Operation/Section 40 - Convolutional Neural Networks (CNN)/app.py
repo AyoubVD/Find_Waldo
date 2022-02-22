9cbe12de64fc45d14bf5daa90333707b53348d0f
AyoubVD <ayoub.disneyti@gmail.com>
2022-02-22 11:01:18 +0100

    Update app.py

--- Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/app.py
+++ Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/app.py
@@ -1,6 +1,7 @@
 # Importing the libraries
 import tensorflow as tf
 from tensorflow.keras.layer import Conv2d
+from tensorflow.keras.layer import MaxPool2d
 from keras.preprocessing.image import ImageDataGenerator
 
 print(tf.__version__)
@@ -36,3 +37,15 @@ cnn = tf.keras.models.Sequential()
 
 # Step 1) Convolution
 cnn.add(Conv2d(filters=32, kernel_size=3, activation='relu', input_shape=(64,64,3)))
+
+# Stap 2) Pooling
+cnn.add(MaxPool2d(pool_size=2, stride=2))
+
+# Adding a second convolutional & pooling layer
+cnn.add(Conv2d(filters=32, kernel_size=3, activation='relu')) # remove input shape, it's only needed with the first layer
+cnn.add(MaxPool2d(pool_size=2, stride=2))
+
+# Stap 3) Flattening 
+# => result of all the previous convolutions and pooling
+# => into a 1D vector which will become the input of a future fully connected neural network
+cnn.add()

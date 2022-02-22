f69b692cf5e9a026e6933ab1e1c9c6fa6153498d
AyoubVD <ayoub.disneyti@gmail.com>
2022-02-22 10:45:01 +0100

    Update app.py

--- Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/app.py
+++ Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/app.py
@@ -1,5 +1,38 @@
 # Importing the libraries
 import tensorflow as tf
+from tensorflow.keras.layer import Conv2d
 from keras.preprocessing.image import ImageDataGenerator
 
 print(tf.__version__)
+
+# Part 1
+# Preprocessing => prevent overfitting/ overtraining
+# Apply transformations
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    sheer_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+training_set = train_datagen.flow_from_directory(
+    'dataset/training_set',
+    target_size=(64,64),
+    batch_size=32,
+    class_mode='binary'
+)
+
+# Preprocessing test set
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory(
+    'dataset/test_set',
+    target_size=64,
+    batch_size=32,
+    class_mode='binary'
+)
+
+# Part 2
+# Building the CNN
+cnn = tf.keras.models.Sequential()
+
+# Step 1) Convolution
+cnn.add(Conv2d())

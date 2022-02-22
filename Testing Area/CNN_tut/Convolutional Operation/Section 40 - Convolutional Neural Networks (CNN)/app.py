0cc24b9656685e3f5efc7864b1e5b6eab3168a31
AyoubVD <ayoub.disneyti@gmail.com>
2022-02-22 12:08:48 +0100

    Update app.py

--- Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/app.py
+++ Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/app.py
@@ -1,22 +1,28 @@
 # Importing the libraries
 import tensorflow as tf
-from tensorflow.keras.layer import Conv2d
-from tensorflow.keras.layer import MaxPool2d
 from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
 
 print(tf.__version__)
+file_path_train = 'C:/Users/ayoub/OneDrive/TMM/Stage fase 3/Arinti/FindWaldo/FindWaldo/Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/dataset/training_set'
+file_path_test = 'C:/Users/ayoub/OneDrive/TMM/Stage fase 3/Arinti/FindWaldo/FindWaldo/Testing Area/CNN_tut/Convolutional Operation/Section 40 - Convolutional Neural Networks (CNN)/dataset/test_set'
 
 # Part 1
 # Preprocessing => prevent overfitting/ overtraining
 # Apply transformations
 train_datagen = ImageDataGenerator(
     rescale=1./255,
-    sheer_range=0.2,
+    shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True
 )
 training_set = train_datagen.flow_from_directory(
-    'dataset/training_set',
+    file_path_train,
     target_size=(64,64),
     batch_size=32,
     class_mode='binary'
@@ -25,27 +31,42 @@ training_set = train_datagen.flow_from_directory(
 # Preprocessing test set
 test_datagen = ImageDataGenerator(rescale=1./255)
 test_set = test_datagen.flow_from_directory(
-    'dataset/test_set',
+    file_path_test,
     target_size=64,
     batch_size=32,
     class_mode='binary'
 )
+
 
 # Part 2
 # Building the CNN
 cnn = tf.keras.models.Sequential()
 
 # Step 1) Convolution
-cnn.add(Conv2d(filters=32, kernel_size=3, activation='relu', input_shape=(64,64,3)))
+cnn.add(tf.keras.layer.Conv2d(filters=32, kernel_size=3, activation='relu', input_shape=(64,64,3)))
 
 # Stap 2) Pooling
-cnn.add(MaxPool2d(pool_size=2, stride=2))
+cnn.add(tf.keras.layer.MaxPool2d(pool_size=2, stride=2))
 
 # Adding a second convolutional & pooling layer
-cnn.add(Conv2d(filters=32, kernel_size=3, activation='relu')) # remove input shape, it's only needed with the first layer
-cnn.add(MaxPool2d(pool_size=2, stride=2))
+cnn.add(tf.keras.layer.Conv2d(filters=32, kernel_size=3, activation='relu')) # remove input shape, it's only needed with the first layer
+cnn.add(tf.keras.layer.MaxPool2d(pool_size=2, stride=2))
 
 # Stap 3) Flattening 
 # => result of all the previous convolutions and pooling
 # => into a 1D vector which will become the input of a future fully connected neural network
-cnn.add()
+cnn.add(tf.keras.layer.Flatten())
+
+# Stap 4) Full Connection
+cnn.add(tf.keras.layer.Dense(units=128, activation='relu')) # units = hidden neurons
+
+# Stap 5) Output Layer
+cnn.add(tf.keras.layer.Dense(units=1, activation='sigmoid'))
+
+
+# Part 3) Training the CNN
+# Compiliing the CNN
+cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Training the CNN on the training set and evaluate it on the test set
+cnn.fit(x = training_set, validation_data=test_set, epochs=25)

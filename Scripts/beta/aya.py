d780c1983f3807e7e8d5d62c45becce11d4e097d
AyoubVD <ayoub.disneyti@gmail.com>
2022-03-16 10:30:20 +0100

    Update aya.py

--- Scripts/beta/aya.py
+++ Scripts/beta/aya.py
@@ -1,2 +1,10 @@
 import cowsay
-cowsay.daemon("Mama Lali")
+
+def helloWorld(x):
+    d = cowsay.get_output_string('daemon', x)
+    t = cowsay.get_output_string('trex', x)
+    return d,t
+
+x,t= helloWorld("yooo")
+print("x")
+
